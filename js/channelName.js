50248c6b800519a0acbf663ba0acbe75d6d9d0d6
Jonathan Baudanza <jon@jonb.org>
2016-09-11 13:38:37 -0700

    display correct channel name

---
+++ js/channelName.js
@@ -0,0 +1,21 @@
+import ip from 'ip-address';
+
+export default function channelName(addressString) {
+  let addr;
+
+  function chomp(addr, delim) {
+    return addr.parsedAddress
+      .slice(0, addr.parsedAddress.length - 1)
+      .join(delim) + delim + '*';
+  }
+
+  addr = new ip.Address6(addressString);
+  if (addr.valid) {
+    return chomp(addr, ':');
+  }
+
+  addr = new ip.Address4(addressString);
+  if (addr.valid) {
+    return chomp(addr, ':');
+  }
+}
