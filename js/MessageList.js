f7b3a1e8fc8277c2f9acff14d66a1462dd81e041
Jonathan Baudanza <jon@jonb.org>
2016-09-11 13:03:19 -0700

    forgot to check this in

---
+++ js/MessageList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import ChatMessage from './ChatMessage';
+
+export default class MessageList extends React.Component {
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate(prevProps: Object, prevState: Object) {
+    if (this.props.messages.length !== prevProps.messages.length) {
+      const ul = this.refs.messageList;
+
+      // If the user is somewhat close to the bottom of the scroll window
+      if (ul.clientHeight + ul.scrollTop + 100 >= ul.scrollHeight)
+        this.scrollToBottom()
+    }
+  }
+
+  scrollToBottom() {
+    const ul = this.refs.messageList;
+    ul.scrollTop = ul.scrollHeight - ul.clientHeight;
+  }
+
+  render() {
+    const style = {
+      overflowY: 'auto',
+      margin: 0,
+      flex: 1,
+      border: '1px solid #eee',
+      padding: '5px'
+    };
+
+    const borderBottom = {borderBottom: '1px solid #eee'};
+
+    return (
+      <ul style={style} ref='messageList'>
+        {
+          this.props.messages.map((e, i) => (
+            <ChatMessage
+                key={e.id}
+                style={i === (this.props.messages.length - 1) ? {} : borderBottom}
+                now={this.props.now}
+                {...e} />
+          ))
+        }
+      </ul>
+    );
+  }
+}
+
+MessageList.propTypes = {
+  messages: React.PropTypes.array.isRequired,
+  now:      React.PropTypes.object.isRequired
+};
