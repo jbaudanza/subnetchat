9f509cfdd88f54cf07770ca3e7b25443897dc5d4
Jonathan Baudanza <jon@jonb.org>
2016-10-25 12:32:20 -0700

    Fix warning by removing chain syntax

--- js/FontAwesome.js
+++ js/FontAwesome.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import _ from 'lodash';
+import {omit} from 'lodash';
 
 function FontAwesome(props: Object): ReactElement {
   return <i
-    {..._(props).omit('icon')}
+    {...omit(props, 'icon')}
     className={`fa fa-${props.icon}`}
   />;
 }
